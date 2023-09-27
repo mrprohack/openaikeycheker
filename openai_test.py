db5ae27e8a4ad551544544b085ac72af0db1b86f
mrprohack <kmani2782@gmail.com>
2023-09-27 16:32:55 +0000

    fix lazy bugs

--- openai_test.py
+++ openai_test.py
@@ -3,30 +3,38 @@ import argparse
 from termcolor import colored
 
 # Function to check if an API key is valid
+
+
 def is_valid_api_key(api_key):
     openai.api_key = api_key
     try:
-        openai.Completion.create(engine='davinci', prompt='Hello', max_tokens=10)
+        openai.Completion.create(
+            engine='davinci', prompt='Hello', max_tokens=10)
         return True
     except openai.error.AuthenticationError:
         return False
     except openai.error.RateLimitError:
-        #print(f"API Key {api_key} exceeded the rate limit. Skipping...")
+        # print(f"API Key {api_key} exceeded the rate limit. Skipping...")
         return False
     except KeyboardInterrupt:
         print("Keyboard interrupt detected. Stopped...")
         exit()
 # Read API keys from a text file
+
+
 def read_api_keys_from_file(filename):
     with open(filename, 'r') as file:
         api_keys = file.read().splitlines()
     return api_keys
 
+
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Check the validity of API keys')
 parser.add_argument('-s', '--single_key', help='API key to check')
-parser.add_argument('-f', '--api_key_file', help='Path to the file containing API keys')
-parser.add_argument('-o', '--output_file', help='Path to save the valid API keys')
+parser.add_argument('-f', '--api_key_file',
+                    help='Path to the file containing API keys')
+parser.add_argument('-o', '--output_file',
+                    help='Path to save the valid API keys')
 args = parser.parse_args()
 
 single_key = args.single_key
@@ -62,11 +70,11 @@ if api_key_file:
     if output_file:
         with open(output_file, 'w') as file:
             file.write('\n'.join(valid_api_keys))
-        print(colored(f"Valid API keys saved to '{output_file}' and Number of valid API keys: {valid_count}.", 'blue'))
+        print(colored(
+            f"Valid API keys saved to '{output_file}' and Number of valid API keys: {valid_count}.", 'blue'))
     else:
         print(colored(f"Number of valid API keys: {valid_count}.", 'blue'))
 
 # Print help menu if no arguments are provided
 if not (single_key or api_key_file):
     parser.print_help()
-
